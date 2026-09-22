f1817191aa8de9a3da0d208174f0ac57bc72218d
Luara <137406346+lualluara@users.noreply.github.com>
2026-09-22 09:25:13 -0300

    adicionamos o pulo

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -4,6 +4,8 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
+    private bool isGrounded = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,11 +18,26 @@ public class Player : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal"); //reconhece o movimento horizontal
         rb.linearVelocity = new Vector2(moveHorizontal *  speed, rb.linearVelocity.y); //adiciona o movimento horizontal
-        if (Input.GetKeyDown(KeyCode.Space))//reconhece quando apertarem a tecla "espaço"
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)//reconhece quando apertarem a tecla "espaço"
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);//Concede o impulso para o player
         }
 
 
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;//Vai comparar parar ver se o jogador está pulando
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false; //Vai reconhecer quando o jogador encostar no chão
+    }
+
+
+
 }
